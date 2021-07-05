446f390bdb1e4a89b93d9660fc9f5d23c3707fec
YoungJun-Lee1994 <dldudwns0402@naver.com>
2021-07-05 14:58:02 +0900

    issue #1

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 #! /usr/bin/env python
+import sys
+print(sys.argv)  #이건 리스트인거고 거기서 원하는 데이터 뽑아내려면 인덱스
 
-print('Hello world')
+
+name = sys.argv[1]
+name1 = sys.argv[2]
+print(f"Hello {name}")
+print(f"Hello {name1}")
+
 
