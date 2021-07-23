44504e65c8b16d87d04d61f11957d9d1c29d1745
YoungJun-Lee1994 <dldudwns0402@naver.com>
2021-07-24 02:30:12 +0900

    210723 exam

---
+++ 210723/210723.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+# # 151
+# l = [3, -20, -3, 44]
+# for i in l:
+#     if i < 0:
+#         print(i)
+
+# 152
+# l = [3, 100, 23, 44]
+# for i in l:
+#     if i % 3 == 0:
+#         print(i)
+
+# 153
+# l = [13, 21, 12, 14, 30, 18]
+# for i in l:
+#     if i % 3 == 0 and i < 20:
+#         print(i)
+
+# 154
+# l = ["I", "study", "python", "language", "!"]
+# for i in l:
+#     if len(i) >= 3:
+#         print(i)
+
+# 155
+# l = ["A", "b", "c", "D"]
+# for i in l:
+#     if i.isupper():
+#         print(i)
+
+# 156
+# for i in l:
+#     if i.islower():
+#         print(i)
+
+
+# # 157
+# l = ["dog", "cat", "parrot"]
+# for i in l:
+#     print(i[0].upper() + i[1:])
+
+# 158
+# l = ["hello.py", "ex01.py", "intro.py"]
+# for i in l:
+#     print(i.split(".")[0])
+
+# # 159
+# l = ["intra.h", "intra.c", "define.h", "run.py"]
+# for i in l:
+#     if i.split(".")[1] == "h":
+#         print(i)
+
+# # 160
+# for i in l:
+#     if i.split(".")[1] == "h" or i.split(".")[1] == "c":
+#         print(i)
