ea999e63de0be73386bce12069d271706b53f27d
YoungJun-Lee1994 <dldudwns0402@naver.com>
2021-07-25 20:42:12 +0900

    210725 practice

---
+++ 210725/210725.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+# 161
+# for i in range(100):
+#     print(i)
+
+# 162
+# for i in range(2002, 2051, 4):
+#     print(i)
+
+# 163
+# for i in range(3, 31, 3):
+#     print(i)
+
+# 164
+# for i in range(99, -1, -1):
+#     print(i)
+
+# 165
+# for i in range(10):
+#     print(f"0.{i}")
+# for i in range(10):
+#     print(i / 10)
+
+# 166
+# for i in range(1, 10):
+#     print(f"3 x {i} = ", 3 * i)
+
+# 167
+# for i in range(1, 10, 2):
+#     print(f"3 x {i} = ", 3 * i)
+
+# 168
+# l = 0
+# for i in range(1, 11):
+#     l += i
+# print(l)
+
+# 169
+# l = 0
+# for i in range(1, 11, 2):
+#     l += i
+# print(l)
+
+# 170
+# l = 1
+# for i in range(1, 11):
+#     l *= i
+# print(l)
